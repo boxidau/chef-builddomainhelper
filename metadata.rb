3b7e042b3d98dd6d4c9a13273fca84e5ec76828e
Simon Mirco <simon.mirco@rackspace.com>
2015-05-05 10:52:44 +1000

    metadata and recipes

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+# Encoding: utf-8
+name 'BuildDomainHelper'
+maintainer 'Simon Mirco'
+maintainer_email 'simon.mirco@rackspace.com'
+license 'Apache 2.0'
+description 'Includes the BuildDominHelper class and methods in recipes'
+version '0.1.0'
